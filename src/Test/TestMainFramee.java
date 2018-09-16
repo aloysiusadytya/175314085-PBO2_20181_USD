a69869bb3506380c2fbad7442dcb2b5048d7b415
Hewlett-Packard <Hewlett-Packard@DESKTOP-AEU90BD>
2018-09-16 11:57:40 +0700

    Pbo2/v7

---
+++ src/Test/TestMainFramee.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import view.MainFrame;
+
+/**
+ *
+ * @author Hewlett-Packard
+ */
+public class TestMainFramee {
+        public static void main(String[] args) {
+        MainFrame test = new MainFrame();
+        test.setSize(600, 400);
+        test.setVisible(true);
+    }
+}
