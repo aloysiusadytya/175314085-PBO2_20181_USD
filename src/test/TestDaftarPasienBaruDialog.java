bddf8ad619a454e522ef0c8a29e60945fa886b97
Hewlett-Packard <Hewlett-Packard@DESKTOP-AEU90BD>
2018-09-25 15:32:13 +0700

    Pbo2/v9

---
+++ src/test/TestDaftarPasienBaruDialog.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Test;
+
+import view.DaftarPasienBaruDialog;
+
+/**
+ *
+ * @author admin
+ */
+public class TestDaftarPasienBaruDialog {
+    public static void main(String[] args) {
+        DaftarPasienBaruDialog test = new DaftarPasienBaruDialog();
+        test.setSize(600, 400);
+        test.setVisible(true);
+    }
+}
